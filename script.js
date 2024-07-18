b78678d3304868908a0623c444ce86697f24057f
shyshy556 <78344050+shyshy556@users.noreply.github.com>
2024-07-18 10:14:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('addForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let result = num1 + num2;
+    document.getElementById('result').textContent = result;
+});
